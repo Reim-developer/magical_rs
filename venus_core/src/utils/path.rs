bd255b081924e55434cfa3675bcfdd187a637b70
Reim-developer <contact.kaxtr@gmail.com>
2025-07-29 19:08:22 +0700

    feat: refactor `get_user_home`, added private `fn` `write_to_file` & export two `PyFunction` `write_to_temp_file` & `get_abs_path`

--- venus_core/src/utils/path.rs
+++ venus_core/src/utils/path.rs
@@ -1,21 +1,63 @@
-use pyo3::pyfunction;
-use std::env::var;
+use pyo3::{PyResult, exceptions::PyIOError, pyfunction};
+use std::fs;
+use std::{
+    env,
+    fs::File,
+    io::Write,
+    path::{self, Path},
+};
 
 #[pyfunction]
 #[must_use]
 pub fn get_user_home() -> Option<String> {
-    #[cfg(target_family = "unix")]
-    {
-        var("HOME").ok()
+    env::home_dir().map(|user_home| user_home.to_string_lossy().to_string())
+}
+
+fn write_to_file(file_path: &str, content: &str) -> PyResult<()> {
+    let file_temp = Path::new(file_path);
+
+    let mut file = File::create_new(file_temp).map_err(|error| {
+        PyIOError::new_err(format!(
+            "Cannot create file: {file_path} with error: {error}"
+        ))
+    })?;
+
+    file.write_all(content.as_bytes()).map_err(|error| {
+        PyIOError::new_err(format!(
+            "Cannot write file: {file_path} with error: {error}"
+        ))
+    })?;
+
+    Ok(())
+}
+
+#[pyfunction]
+/// # Errors
+/// Will be convert to Python Exception
+pub fn write_to_temp_file(file_path: &str, content: &str) -> PyResult<()> {
+    let file_temp = Path::new(file_path);
+
+    if !file_temp.exists() {
+        write_to_file(file_path, content)?;
     }
 
-    #[cfg(target_family = "windows")]
-    {
-        var("USERPROFILE").ok()
+    if file_temp.exists() {
+        fs::remove_file(file_temp).map_err(|error| {
+            PyIOError::new_err(format!(
+                "Could not remove file {file_path} with error: {error}"
+            ))
+        })?;
+
+        write_to_file(file_path, content)?;
     }
 
-    #[cfg(not(any(target_family = "unix", target_family = "windows")))]
-    {
-        None
-    }
+    Ok(())
+}
+
+#[pyfunction]
+#[must_use]
+pub fn get_abs_path(file_path: &str) -> Option<String> {
+    path::absolute(file_path).map_or(None, |file_str| {
+        Some(file_str.to_string_lossy().to_string())
+    })
 }

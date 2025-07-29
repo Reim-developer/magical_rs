b2ea96e41ef55ed7845dda0a1a7fb1472ac01182
Reim-developer <contact.kaxtr@gmail.com>
2025-07-29 22:01:23 +0700

    feat: added class `VenusAbout`

--- venus_gui/gui/__init__.py
+++ venus_gui/gui/__init__.py
@@ -1,5 +1,6 @@
 from gui.main_window import VenusMainWindow
 from gui.open_file import OpenFile
 from gui.file_dialog import FileDialog
+from gui.venus_info import VenusAbout
 
-__all__ = ["VenusMainWindow", "OpenFile", "FileDialog"]
+__all__ = ["VenusMainWindow", "OpenFile", "FileDialog", "VenusAbout"]

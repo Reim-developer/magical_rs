dc5982410bac12fce8606b2e64a01cbd0ddb63c1
Reim-developer <contact.kaxtr@gmail.com>
2025-07-29 19:08:33 +0700

    feat: added match case for `cli` mode

--- venus_gui/main.py
+++ venus_gui/main.py
@@ -5,15 +5,31 @@ from gui import VenusMainWindow
 def main_launch() -> None:
     app = QApplication(argv)
 
-    if len(argv) > 1 and argv[1] == "--debug":
-        venus_main_window = VenusMainWindow(is_debug = True)
+    if len(argv) > 1:
+        match argv[1]:
+            case "--debug":
+                venus_main_window = VenusMainWindow(is_debug = True)
+                venus_main_window.show_gui()
+                exit(app.exec())
+
+            case "--verbose":
+                venus_main_window = VenusMainWindow(is_verbose = True)
+                venus_main_window.show_gui()
+
+                exit(app.exec())
+
+            case "--dev-mode":
+                venus_main_window = VenusMainWindow(is_debug = True, is_verbose = True)
+                venus_main_window.show_gui()
+                
+                exit(app.exec())
+                
+            case _: ...
 
     else:
-        venus_main_window = VenusMainWindow()
-
-    venus_main_window.show_gui()
-
-    exit(app.exec())
+        venus_main_window  = VenusMainWindow()
+        venus_main_window.show_gui()
+        exit(app.exec())
 
 if __name__ == "__main__":
     main_launch()

deeef40ac30d483fe861c9a0b6a386a4930e1f95
Reim-developer <contact.kaxtr@gmail.com>
2025-07-29 09:46:48 +0700

    feat: Added class `AppSignals` & `SetupSignals`

--- venus_gui/gui/__init__.py
+++ venus_gui/gui/__init__.py
@@ -1,4 +1,6 @@
 from gui.main_window import VenusMainWindow
 from gui.menu_bar import MenuBar
+from gui.signals import AppSignals
+from gui.setup_signals import SetupSignals
 
-__all__ = ["VenusMainWindow", "MenuBar"]
+__all__ = ["VenusMainWindow", "MenuBar", "AppSignals", "SetupSignals"]

678bc2ce06d2527b555de6edfc05e74fc4a314bc
Reim-developer <contact.kaxtr@gmail.com>
2025-08-01 03:52:10 +0700

    feat: + Implement for  struct `Magic`

--- crate/magical-rs/src/magical/signatures.rs
+++ crate/magical-rs/src/magical/signatures.rs
@@ -2,7 +2,9 @@ use crate::magical::bytes_read::{
     DEFAULT_MAX_BYTES_READ, DEFAULT_OFFSET, ISO_MAX_BYTES_READ, ISO_OFFSETS, TAR_MAX_BYTES_READ,
     TAR_OFFSETS,
 };
+use crate::magical::ext_fn::webp::is_webp;
 use crate::magical::magic::FileKind;
+use crate::magical::match_rules::MatchRules;
 
 const PNG_SIGNATURE: &[u8] = &[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 const GZIP_SIGNATURE: &[u8] = &[0x1F, 0x8B];
@@ -49,12 +51,30 @@ const CREATIVE_VOICE_FILE_SIGNATURE: &[u8] = &[
 ];
 const AU_AUDIO_FILE_FORMAT_SIGNATURE: &[u8] = &[0x2E, 0x73, 0x6E, 0x64];
 const OPENGL_IRIS_PERFORMER_SIGNATURE: &[u8] = &[0xDB, 0x0A, 0xCE, 0x00];
-
+const NOODLESOFT_HAZEL_SIGNATURE: &[u8] = &[0x48, 0x5A, 0x4C, 0x52, 0x00, 0x00, 0x00, 0x18];
+const VB_SCRIPT_ENCODED_SIGNATURE: &[u8] = &[0x23, 0x40, 0x7E, 0x5E];
 pub struct Magic {
     pub signatures: &'static [&'static [u8]],
     pub offsets: &'static [usize],
     pub max_bytes_read: usize,
     pub kind: FileKind,
+    pub rules: MatchRules,
+}
+
+impl Magic {
+    #[must_use]
+    pub fn matches(&self, bytes: &[u8]) -> bool {
+        match &self.rules {
+            MatchRules::Default => self.signatures.iter().any(|&signature| {
+                self.offsets.iter().any(|&offset| {
+                    let offset_end = offset + signature.len();
+
+                    bytes.len() >= offset_end && &bytes[offset..offset_end] == signature
+                })
+            }),
+            MatchRules::WithFn(func) => func(bytes),
+        }
+    }
 }
 
 pub static SIGNATURE_KIND: &[Magic] = &[
@@ -63,191 +83,244 @@ pub static SIGNATURE_KIND: &[Magic] = &[
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Png,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[CLASS_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Class,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[JPG_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Jpg,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[GZIP_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Gzip,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[BZIP_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Bzip,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[PKG_ZIP_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::PkgZip,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[BITMAP_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Bitmap,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[MS_DOS_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::MSDOS,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[TAR_SIGNATURE],
         offsets: TAR_OFFSETS,
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Tar,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[MP3_SIGNATURE_1, MP3_SIGNATURE_2, MP3_SIGNATURE_3],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: TAR_MAX_BYTES_READ,
         kind: FileKind::MP3,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[ISO_SIGNATURE],
         offsets: ISO_OFFSETS,
         max_bytes_read: ISO_MAX_BYTES_READ,
         kind: FileKind::ISO,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[RPM_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::RPM,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[SQLITE_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::SQLite,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[XML_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::XML,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[ICO_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::ICO,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[WASM_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::WASM,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[DEB_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Deb,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[SCRIPT_EXECUTE_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::ScriptExecute,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: RAR_SIGNATURE,
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::RAR,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[ELF_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::ELF,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[OGG_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::OGG,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[_8BPS_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::_8BPS,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[BLENDER_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::BLENDER,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[TRUE_TYPE_FONT_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::TrueTypeFont,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[OPEN_TYPE_FONT_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::OpenTypeFont,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[MODULEFILE_FOR_ENVIRONMENT_MODULES_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::ModuleForEvenvironmentModules,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[WINDOW_IMAGING_FORMAT_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::WindowImagingFormat,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[SLOB_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::Slob,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[SERIALIZED_JAVA_DATA_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::SerializedJavaData,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[CREATIVE_VOICE_FILE_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::CreativeVoiceFile,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[AU_AUDIO_FILE_FORMAT_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::AuAudioFileFormat,
+        rules: MatchRules::Default,
     },
     Magic {
         signatures: &[OPENGL_IRIS_PERFORMER_SIGNATURE],
         offsets: &[DEFAULT_OFFSET],
         max_bytes_read: DEFAULT_MAX_BYTES_READ,
         kind: FileKind::OpenGLIrisPerformer,
+        rules: MatchRules::Default,
+    },
+    Magic {
+        signatures: &[NOODLESOFT_HAZEL_SIGNATURE],
+        offsets: &[DEFAULT_OFFSET],
+        max_bytes_read: DEFAULT_MAX_BYTES_READ,
+        kind: FileKind::NoodlesoftHazel,
+        rules: MatchRules::Default,
+    },
+    Magic {
+        signatures: &[VB_SCRIPT_ENCODED_SIGNATURE],
+        offsets: &[DEFAULT_OFFSET],
+        max_bytes_read: DEFAULT_MAX_BYTES_READ,
+        kind: FileKind::VBScriptEncoded,
+        rules: MatchRules::Default,
+    },
+    Magic {
+        signatures: &[],
+        offsets: &[],
+        max_bytes_read: DEFAULT_MAX_BYTES_READ,
+        kind: FileKind::WEBP,
+        rules: MatchRules::WithFn(is_webp),
     },
 ];

d998187cfdf5ed381539399ea39403cc48978c2f
Reim-developer <contact.kaxtr@gmail.com>
2025-08-01 03:50:19 +0700

    feat: + Added test case with `Class` & `WEBP` file type

--- crate/magical-rs/tests/test.rs
+++ crate/magical-rs/tests/test.rs
@@ -14,5 +14,52 @@ fn test_iso_detect() {
     let wrong_header = read_file_header(iso_file, wrong_max).unwrap();
 
     assert_eq!(FileKind::match_types(&header_bytes), FileKind::ISO);
+    assert_ne!(FileKind::match_types(&header_bytes), FileKind::Unknown);
     assert_ne!(FileKind::match_types(&wrong_header), FileKind::ISO);
+    assert_eq!(FileKind::match_types(&wrong_header), FileKind::Unknown);
+}
+
+#[test]
+fn test_png_detect() {
+    use magical_rs::magical::{
+        bytes_read::{read_file_header, with_bytes_read},
+        magic::FileKind,
+    };
+
+    let png_file = "tests/1.png";
+    let bytes_max = with_bytes_read();
+    let header_bytes = read_file_header(png_file, bytes_max).unwrap();
+
+    assert_eq!(FileKind::match_types(&header_bytes), FileKind::Png);
+    assert_ne!(FileKind::match_types(&header_bytes), FileKind::Unknown);
+}
+
+#[test]
+fn test_class_detect() {
+    use magical_rs::magical::{
+        bytes_read::{read_file_header, with_bytes_read},
+        magic::FileKind,
+    };
+
+    let png_file = "tests/3.class";
+    let bytes_max = with_bytes_read();
+    let header_bytes = read_file_header(png_file, bytes_max).unwrap();
+
+    assert_eq!(FileKind::match_types(&header_bytes), FileKind::Class);
+    assert_ne!(FileKind::match_types(&header_bytes), FileKind::Unknown);
+}
+
+#[test]
+fn test_webp_detect() {
+    use magical_rs::magical::{
+        bytes_read::{read_file_header, with_bytes_read},
+        magic::FileKind,
+    };
+
+    let webp_file = "tests/4.webp";
+    let bytes_max = with_bytes_read();
+    let header_bytes = read_file_header(webp_file, bytes_max).unwrap();
+
+    assert_eq!(FileKind::match_types(&header_bytes), FileKind::WEBP);
+    assert_ne!(FileKind::match_types(&header_bytes), FileKind::Unknown);
 }

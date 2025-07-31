f2be2f1a6e69d7834a2231c1e1220da5024a7a0e
Reim-developer <contact.kaxtr@gmail.com>
2025-07-31 14:44:21 +0700

    feat: added file mime value constant

---
+++ venus_core/src/mime_types/file_mime.rs
@@ -0,0 +1,8 @@
+pub const PNG_MIME_TYPE: &str = "image/png";
+pub const BITMAP_MIME_TYPE: &str = "image/bmp";
+pub const GZIP_MIME_TYPE: &str = "application/gzip";
+pub const BZIP_MIME_TYPE: &str = "application/x-bzip2";
+pub const PKG_ZIP_MIME_TYPE: &str = "application/zip";
+pub const TAR_MIME_TYPE: &str = "application/x-tar";
+pub const MS_DOS_MIME_TYPE: &str = "application/x-msdos-program";
+pub const UNKNOWN_MIME_TYPE: &str = "unknown";

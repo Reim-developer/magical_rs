1e03ee9f32c2255a64672176a25956986fac370f
Reim-developer <contact.kaxtr@gmail.com>
2025-08-09 08:53:26 +0700

    Added testing for the above features

--- tests/unsafe.rs
+++ tests/unsafe.rs
@@ -39,3 +39,98 @@ fn test_unsafe_magic_custom() {
     assert_eq!(result, MagicKind::MoeMoe);
     assert_ne!(result, MagicKind::UnknownFallback);
 }
+
+#[test]
+#[cfg(feature = "unsafe_context")]
+fn test_any_unsafe_fn() {
+    use core::slice;
+    use magical_rs::magical::magic_custom::match_types_custom;
+    use magical_rs::magical::magic_custom::{CustomMatchRules, MagicCustom};
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    enum MagicKind {
+        MoeMoe,
+        UnknownFallback,
+    }
+
+    fn is_shoujo_girl(data: *const ()) -> bool {
+        unsafe {
+            let slice_ptr = data.cast::<u8>();
+            let slice = slice::from_raw_parts(slice_ptr, 100);
+
+            slice.starts_with(b"MagicalGirl")
+        }
+    }
+
+    fn is_not_shoujo_girl(data: *const ()) -> bool {
+        unsafe {
+            let slice_ptr = data.cast::<u8>();
+            let slice = slice::from_raw_parts(slice_ptr, 100);
+
+            !slice.starts_with(b"MagicalGirl")
+        }
+    }
+
+    let rules: &[MagicCustom<MagicKind>] = &[MagicCustom {
+        signatures: &[],
+        offsets: &[],
+        max_bytes_read: 200,
+        kind: MagicKind::MoeMoe,
+        /*
+         * We just need to clarify that, even though these two
+         * functions are logically contradictory, they can still
+         * be crossed using logical `OR`
+         */
+        rules: CustomMatchRules::AnyMatchesUnsafe(&[is_shoujo_girl, is_not_shoujo_girl]),
+    }];
+
+    let result = match_types_custom(b"MagicalGirl", rules, MagicKind::UnknownFallback);
+
+    assert_eq!(result, MagicKind::MoeMoe);
+    assert_ne!(result, MagicKind::UnknownFallback);
+}
+
+#[test]
+#[cfg(feature = "unsafe_context")]
+fn test_all_unsafe_fn() {
+    use core::slice;
+    use magical_rs::magical::magic_custom::match_types_custom;
+    use magical_rs::magical::magic_custom::{CustomMatchRules, MagicCustom};
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    enum MagicKind {
+        MoeMoe,
+        UnknownFallback,
+    }
+
+    fn is_shoujo_girl(data: *const ()) -> bool {
+        unsafe {
+            let slice_ptr = data.cast::<u8>();
+            let slice = slice::from_raw_parts(slice_ptr, 100);
+
+            slice.starts_with(b"MagicalGirl")
+        }
+    }
+
+    fn is_not_shoujo_girl(data: *const ()) -> bool {
+        unsafe {
+            let slice_ptr = data.cast::<u8>();
+            let slice = slice::from_raw_parts(slice_ptr, 100);
+
+            !slice.starts_with(b"MagicalGirl")
+        }
+    }
+
+    let rules: &[MagicCustom<MagicKind>] = &[MagicCustom {
+        signatures: &[],
+        offsets: &[],
+        max_bytes_read: 200,
+        kind: MagicKind::MoeMoe,
+        rules: CustomMatchRules::AllMatchesUnsafe(&[is_shoujo_girl, is_not_shoujo_girl]),
+    }];
+
+    let result = match_types_custom(b"MagicalGirl", rules, MagicKind::UnknownFallback);
+
+    assert_ne!(result, MagicKind::MoeMoe);
+    assert_eq!(result, MagicKind::UnknownFallback);
+}
